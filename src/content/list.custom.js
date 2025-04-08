2862b3effa69fa2b20ab1d196857f992a6047ea6
Michael Urspringer <michael@urspringer.de>
2025-04-08 15:40:43 +0200

    Resolve issue https://github.com/sternth/no-gender/issues/27

--- src/content/list.custom.js
+++ src/content/list.custom.js
@@ -10,5 +10,8 @@
      */
     { regExp: /jede[:*_]r/, replace: 'jeder' },
     { regExp: /ein[:*_]e/, replace: 'ein' },
+    { regExp: /Sinti\*zze/, replace: 'Sinti' },
+    { regExp: /Sint\*ezza/, replace: 'Sinti' },
+
   ]
 }(typeof window !== 'undefined' ? window : this))
